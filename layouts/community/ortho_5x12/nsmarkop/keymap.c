9990e112a82f390619f6ab5862ef3d4fd07a370c
Nicholas Markopoulos <nsmarkop@gmail.com>
2018-03-12 21:37:11 -0500

    Add fourths layer similar to linnstrument

--- layouts/community/ortho_5x12/nsmarkop/keymap.c
+++ layouts/community/ortho_5x12/nsmarkop/keymap.c
@@ -13,7 +13,8 @@ enum custom_layers {
   _RAISE,
   _ADJUST,
   _MIDI,
-  _SERIAL,
+  _FOURTHS
+  // TODO: _SERIAL
 };
 
 enum custom_keycodes {
@@ -22,7 +23,8 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   MIDI,
-  SERIAL
+  FOURTHS
+  // TODO: SERIAL
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -61,11 +63,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_ADJUST] = KEYMAP(
   RESET,   MAGIC_TOGGLE_NKRO, _______, _______, _______, _______,    _______, _______, _______, _______, _______,  _______,
-  _______, _______,           _______, _______, _______, _______,    _______, _______, _______, _______, _______,  _______,
+  _______, _______,           _______, _______, _______, _______,    _______, FOURTHS, _______, _______, _______,  _______,
   _______, _______,           _______, _______, _______, _______,    _______, MIDI,    QWERTY,  KC_RALT, KC_RCTRL, _______,
   _______, _______,           _______, _______, _______, _______,    _______, _______, _______, _______, _______,  _______,
   _______, _______,           _______, _______, _______, _______,    _______, _______, _______, _______, _______,  _______
 ),
+
+// NOTE: At this point in time, QMK's MIDI values are 2 off from what they actually output
+//       for some reason. May have to look into some of the MIDI_ADVANCED config for that
 
 [_MIDI] = KEYMAP(
   MI_C,   MI_Cs,   MI_D,   MI_Ds,   MI_E,   MI_F,      MI_Fs,   MI_G,   MI_Gs,   MI_A,   MI_As,   MI_B,
@@ -73,6 +78,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MI_C_2, MI_Cs_2, MI_D_2, MI_Ds_2, MI_E_2, MI_F_2,    MI_Fs_2, MI_G_2, MI_Gs_2, MI_A_2, MI_As_2, MI_B_2,
   MI_C_3, MI_Cs_3, MI_D_3, MI_Ds_3, MI_E_3, MI_F_3,    MI_Fs_3, MI_G_3, MI_Gs_3, MI_A_3, MI_As_3, MI_B_3,
   MI_C_4, MI_Cs_4, MI_D_4, MI_Ds_4, MI_E_4, MI_F_4,    MI_Fs_4, MI_G_4, MI_Gs_4, MI_A_4, MI_As_4, MIDI
+),
+
+[_FOURTHS] = KEYMAP(
+  MI_Cs_3, MI_D_3,  MI_Ds_3, MI_E_3,  MI_F_3, MI_Fs_3,    MI_G_3, MI_Gs_3, MI_A_3,  MI_As_3, MI_B_3,  MI_C_4,
+  MI_Gs_2, MI_A_2,  MI_As_2, MI_B_2,  MI_C_3, MI_Cs_3,    MI_D_3, MI_Ds_3, MI_E_3,  MI_F_3,  MI_Fs_3, MI_G_3,
+  MI_Ds_2, MI_E_2,  MI_F_2,  MI_Fs_2, MI_G_2, MI_Gs_2,    MI_A_2, MI_As_2, MI_B_2,  MI_C_3,  MI_Cs_3, MI_D_3,
+  MI_As_1, MI_B_1,  MI_C_2,  MI_Cs_2, MI_D_2, MI_Ds_2,    MI_E_2, MI_F_2,  MI_Fs_2, MI_G_2,  MI_Gs_2, MI_A_2,
+  MI_F_1,  MI_Fs_1, MI_G_1,  MI_Gs_1, MI_A_1, MI_As_1,    MI_B_1, MI_C_2,  MI_Cs_2, MI_D_2,  MI_Ds_2, FOURTHS
 )
 
 // TODO:
@@ -131,6 +144,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MIDI:
       if (record->event.pressed) {
         layer_invert(_MIDI);
+      }
+      sendKeypress = false;
+      break;
+    case FOURTHS:
+      if (record->event.pressed) {
+        layer_invert(_FOURTHS);
       }
       sendKeypress = false;
       break;
